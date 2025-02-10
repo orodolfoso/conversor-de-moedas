62a796bf88ada48be802d0d2356ee64e0ff58f0b
orodolfoso <orodolfoso.dev@gmail.com>
2025-02-09 22:08:56 -0300

    Concluindo o conversor no javascript

--- scripts.js
+++ scripts.js
@@ -1,13 +1,39 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues () {
-    const inputCurrencyValue = document.querySelector(".input-currency").value 
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueToConverted = document.querySelector(".currency-value")
 
-    const dollarToday = 5.77
+    const dollarToday = 5.81
+    const eurotoday = 5.99
 
-    const convertValues = inputCurrencyValue / dollarToday
+    if (currencySelect.value == "dolar") {
 
-    console.log(convertValues)
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dollarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / eurotoday)
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
+
 }
 
 convertButton.addEventListener("click", convertValues)
