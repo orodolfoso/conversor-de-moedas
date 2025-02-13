ba24ce6ca14b0cb4f0cdce7fe457dab1451d1265
orodolfoso <orodolfoso.dev@gmail.com>
2025-02-13 15:53:34 -0300

    Atualizando

--- scripts.js
+++ scripts.js
@@ -1,41 +1,28 @@
 
 const convertButton = document.querySelector('.convert-button')
-const currencySelect = document.querySelector('.currency-select')
 
-function inputConvertValue(){
-    const inputCurrencyValue = document.querySelector('.input-currency').value
-    const currencyValueToConvert = document.querySelector('.currency-value-to-convert')
-    const currencyValueConverted = document.querySelector('.currency-value')
 
-    console.log(currencySelect.value)
-
-    if (currencySelect.value == 'dolar') {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD'
-           }).format(inputCurrencyValue/ dolarToday)
-    }
-
-    if(currencySelect.value == 'euro') {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat('eu-US', {
-            style: 'currency',
-            currency: 'USD'
-           }).format(inputCurrencyValue /  euroToday)
-    }
-    
+function convertValues() {
+    const inputToConvert = document.querySelector('.input-currency').value 
+    const converteCurrencyValue = document.querySelector('.currency-value-to-convert')
+    const convertedValue = document.querySelector('.currency-value')
 
     const dolarToday = 5.75
-    const euroToday = 6.70
 
-   currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-   }).format(inputCurrencyValue)
+    const currencyConvertValue = inputToConvert / dolarToday
 
+    converteCurrencyValue.innerHTML =  new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(inputToConvert)
 
-   
-    
+    convertedValue.innerHTML =   new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(currencyConvertValue)
 }
 
-convertButton.addEventListener('click', inputConvertValue)
+
+
+convertButton.addEventListener('click', inputToConvert )
 
